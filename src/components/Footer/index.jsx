66e7d566b03246daf8228db82d05c32dc76833d9
Raynxxx <rayn1027@outlook.com>
2016-10-09 17:34:45 +0800

    update Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import './style.less';
 
 export default class Footer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      today: new Date()
+    };
+  }
+
   render() {
     return (
       <footer className="layout-footer">
-        <p>CUIT ACM Team</p>
+        <p>CUIT ACM Team © 2011 - {this.state.today.getFullYear()}</p>
       </footer>
     );
   }
